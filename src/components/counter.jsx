a53a23537d8b367c75f3bb20c1bc598230cf151b
Colin Huber <cbhuber@gmail.com>
2020-12-30 14:51:09 -0700

    feat: Added reset button and refactor code.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: 0,
-  };
+  // state = {
+  //   value: this.props.counter.value,
+  // };
 
   //   constructor() {
   //     //  Super must be called to access "this"
@@ -16,9 +16,9 @@ class Counter extends Component {
 
   // Arrow notation allows "this" to be accessed without having to use the commented out constructor above
   // Then it would prevent the noise of binding "this" to every new function made.
-  handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
+  // handleIncrement = () => {
+  //   this.setState({ value: this.state.value + 1 });
+  // };
 
   // Using bootstrap classes
   render() {
@@ -26,10 +26,16 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
@@ -37,13 +43,13 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
 }
 
